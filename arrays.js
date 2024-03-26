05034cff44ba54ae49ab4476f393eb9a0f2b808b
DonPablo7 <144667847+DonPablo7@users.noreply.github.com>
2024-03-26 13:44:53 +0100

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,15 @@
+// ---------------- zadanie nr 1
+
+// const fruits = ['Jablko', 'Banan', 'Pomarancza', 'Winogrona', 'Arbuz'];
+// console.log(fruits)
+
+// ---------------- zadanie nr 2
+
+// const numbers = [0, 1, 2, 5, 7, 8, 9, 12, 15, 21];
+
+// const wyznacznik = 5;
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > wyznacznik) {
+//         console.log(numbers[i])
+//     }
+// }
